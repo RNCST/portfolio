b7cf94168efa37adfe2f735a1f6820c0dea3a918
RNCST <76672259+RNCST@users.noreply.github.com>
2021-06-15 23:55:46 +0900

    21/06/15 portpolio reset

--- react_template/book-app/src/components/book.jsx
+++ react_template/book-app/src/components/book.jsx
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
 
-
 class Book extends Component {
-   state = {
-      count: 0,
-   };
-   eventIncrement = (event) => {
-      console.log(event);
-      //this.state.count++;
-      this.setState({ count: this.state.count + 1 })
-   }
-   eventDecrement = (event) => {
-      console.log(event);
-      this.setState({ count: this.state.count - 1 })
-   }
-   render() {
-      return (
-         <li className="book">
-            <span className="book-name">자바프로그래밍</span>
-            <span className="book-count">{this.state.count}</span>
-            <button className="book-button book-increase" onClick={this.eventIncrement}>
-               <i className="fas fa-plus"></i>
-            </button>
-            <button className="book-button book-decrease" onClick={this.eventDecrement}>
-               <i className="fas fa-minus"></i>
-            </button>
-         </li>
-      )
-   }
-};
+  eventIncrement = () => {
+    this.props.onIncrement(this.props.book);
+  }
+  eventDecrement = () => {
+    this.props.onDecrement(this.props.book);
+  }
+  eventDelete = () => {
+    this.props.onDelete(this.props.book);
+  }
+  render() {
+    //console.log(this.props.book);
+    const { name, count } = this.props.book;
+    return (
+      <li className="book">
+        <span className="book-name">{name}</span>
+        <span className="book-count">{count}</span>
+        <button className="book-button book-increase" onClick={this.eventIncrement}>
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button className="book-button book-decrease" onClick={this.eventDecrement}>
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button className="book-button book-zero" onClick={this.eventDelete}>
+          <i className="fas fa-trash-alt"></i>
+        </button>
+      </li>
+    );
+  }
+}
 
 export default Book;
--- react_template/book-app/src/index.js
+++ react_template/book-app/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app.jsx';
+import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
-
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,33 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// const { of, interval, range, EMPTY } = Rx;
-// const { delay, take, map, concatMap } = RxOperators;
-
-// const sentence = new Date().toString().toUpperCase();
-// const words = sentence.split(' ');
-// const delayMS = 1000;
-
-// const wordDelay = i =>
-//   i === 0
-//     ? delayMS
-//     : (words[i - 1].length + 1) * delayMS;
-
-// const wordStart = i =>
-//   i < words.length
-//     ? of(i).pipe(delay(wordDelay(i)))
-//     : EMPTY.pipe(delay(wordDelay(i)))
-
-// const wordObservable = word => {
-//   const letters = word.split('');
-
-//   return interval(delayMS).pipe(
-//     take(letters.length),
-//     map(i => letters[i])
-//   );
-// };
-
-// range(0, words.length + 1).pipe(
-//   concatMap(wordStart),
-//   map(i => wordObservable(words[i]))
-// );
